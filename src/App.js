f8c1e6e555034da4c2d2f29bdeb83d7eb4ec6051
Emmanuel Kofi Agyapong <agiapong@gmail.com>
2021-11-19 07:55:05 -0500

    final commit, I will become a React pro!

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/TheWizard91"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Github Profile: https://github.com/TheWizard91
         </a>
       </header>
     </div>
