227d66a1f82f8010ad59317e03e171a535d89d25
Emmanuel Kofi Agyapong <agiapong@gmail.com>
2021-11-19 06:45:00 -0500

    modified App.js to day {insert your name here}

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>My name is Emmanuel and I am going to master react!</code>
         </p>
         <a
           className="App-link"
